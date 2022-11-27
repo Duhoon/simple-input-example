6e0252bfd63a66e3732d6c7e7ffbc9f700f870ba
Duhoon <eclip6@naver.com>
2022-11-27 14:31:57 +0900

    before applying redux

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -47,7 +47,7 @@ const Detail = (props)=>{
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 inline">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
                         </svg>
-                        <span className="font-bold">Back</span>
+                        <span className="font-bold">Go Back</span>
                     </div>
                 </button>
             </div>
